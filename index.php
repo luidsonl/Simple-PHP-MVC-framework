34f533859a6ac9a5aaa053ea8b961445a9642d30
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-03-16 22:52:14 -0300

    Creating Application and Router; Enabling autoloading using composer; Implement Routing

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+$app->router->get('/', function(){
+	return 'Hello World';
+});
+
+
+$app->run();
+?>
