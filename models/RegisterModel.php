c397088abc8cd8f0a4df94ae28ea9e92e8966735
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-04-22 15:55:01 -0300

    (02:00:21) Implement form Widget

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -12,5 +12,15 @@ class RegisterModel extends Model{
 	public function register(){
 		echo "creating new user";
 	}
+
+	public function rules(): array{
+		return[
+			'firstname'			=> [self::RULE_REQUIRED],
+			'lastname' 			=> [self::RULE_REQUIRED],
+			'email'				=> [self::RULE_REQUIRED, self::RULE_EMAIL],
+			'password'			=> [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
+			'confirmPassword'	=> [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+		];
+	}
 }
 ?>
