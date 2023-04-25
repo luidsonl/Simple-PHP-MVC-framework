044cf40d02968a2539db2f2e8f709eb66e93728f
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-04-25 20:13:05 -0300

    (03:23:56) Restrict registration with the same email

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,7 +2,7 @@
 namespace app\controllers;
 use app\core\Controller;
 use app\core\Request;
-use app\models\RegisterModel;
+use app\models\User;
 
 class AuthController extends Controller{
 
@@ -13,31 +13,31 @@ class AuthController extends Controller{
 
 	public function register(Request $request){
 
-		$registerModel = new RegisterModel();
+		$user = new User();
 
 		if ($request->isPost()){
-			$registerModel->loadData($request->getBody());
+			$user->loadData($request->getBody());
 
-			if ($registerModel->validate() && $registerModel->register()){
+			if ($user->validate() && $user->save()){
 				return 'Success';
 			}
 
 			/*
 			echo '<pre>';
-			var_dump($registerModel->errors);
+			var_dump($user->errors);
 			echo '</pre>';
 			exit();
 			*/
 
 			return $this->render('register', [
-				'model' => $registerModel
+				'model' => $user
 			]);
 		}
 
 		$this->setLayout('auth');
 
 		return $this->render('register',[
-			'model' => $registerModel
+			'model' => $user
 		]);
 	}
 }
--- models/User.php
+++ models/User.php
@@ -1,16 +1,28 @@
 <?php
 namespace app\models;
-use app\core\Model;
+use app\core\DbModel;
 
-class RegisterModel extends Model{
+class User extends DbModel{
+	const STATUS_INACTIVE = 0;
+	const STATUS_ACTIVE = 1;
+	const STATUS_DELETED = 2;
+
 	public string $firstname = '';
 	public string $lastname = '';
 	public string $email = '';
+	public int $status = self::STATUS_INACTIVE;
 	public string $password = '';
 	public string $confirmPassword = '';
 
-	public function register(){
-		echo "creating new user";
+	public function tableName(): string{
+		return 'users';
+	}
+
+	public function save(){
+		$this->status = self::STATUS_INACTIVE;
+		$this->password = password_hash($this->password, PASSWORD_DEFAULT);
+		return parent::save();
+		
 	}
 
 	public function rules(): array{
@@ -21,6 +33,9 @@ class RegisterModel extends Model{
 			'password'			=> [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
 			'confirmPassword'	=> [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
 		];
+	}
+	public function attributes(): array{
+		return ['firstname', 'lastname', 'email', 'password', 'status'];
 	}
 }
 ?>
